f589101c898ca27c9dbbcbb15cf16fc7caa2f646
LigeiaR <chkonstad@gmail.com>
2019-07-10 19:16:35 +0200

    add python file

--- allspec.py
+++ allspec.py
@@ -1,14 +1,16 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Feb  8 11:29:04 2019
+#!/usr/bin/env python
+# coding: utf-8
 
-@author: Christina
-"""
+# In[9]:
+
+
+
 
 #Libraries for plotting
 import matplotlib
 import matplotlib.pyplot as plt
+from matplotlib.ticker import ScalarFormatter
+get_ipython().magic(u'matplotlib inline')
 
 #Standard numerical and scientific libraries
 import numpy as np
@@ -27,22 +29,28 @@ from specutils.io import read_fits
 from PyAstronomy import pyasl
 from specutils import extinction
 
-import sfdmap  #for the SFD dust maps 
+import sfdmap  # SFD dust maps 
 
 from decimal import Decimal #convert to decimals
 
-#import pyfits
 from string import rstrip
 
 from matplotlib import rc   #for Latex text
 
-#Load files:
-mags = np.loadtxt("/home/christina/Desktop/QSOs/mags.list")
-comp_file=np.loadtxt('/home/christina/Desktop/QSOs/compoM.data')
+
+# Load files
+mags = np.loadtxt("/home/christina/Desktop/QSOs/mags.list")     #magnitude list
+comp_file=np.loadtxt('/home/christina/Desktop/QSOs/compoM.data')   #composite spectrum
 zab = np.loadtxt("/home/christina/Desktop/QSOs/zab1.list")
 names = np.loadtxt("/home/christina/Desktop/QSOs/names.list", dtype=np.str)
 
-#Read the coordinates:
+AB=zab[:,1]
+z=zab[:,0]
+
+wlr=comp_file[:,0]/1e4   #wavelength of composite spectrum in microns  (rest frame)
+flx=comp_file[:,1]   #flux of composite spectrum
+
+# Read the coordinates and estimate ebv values:
 f = open("/home/christina/Desktop/QSOs/coord.list", "r")
 e=open("/home/christina/Desktop/QSOs/ebv.list", "w")    #open file to write 
 rah,decd = [], []
@@ -62,20 +70,16 @@ for l in f:
 e.close()
 
 ebv = np.loadtxt('/home/christina/Desktop/QSOs/ebv.list')
-AB=zab[:,1]
-z=zab[:,0]
 
-wlr=comp_file[:,0]/1e4   #wavelength of composite spectrum in microns  (rest frame)
-flx=comp_file[:,1]   #flux of composite spectrum
 
-#Parameters for the Pei-parametrization of the SMC extinction curve
+# Parameters for the Pei-parametrization of the SMC extinction curve
 ai = np.array([185.,27.,0.005,0.010,0.012,0.030])
 wli = np.array([0.042,0.08,0.22,9.7,18.,25.])
 bi = np.array([90.,5.50,-1.95,-1.95,-1.80,0.0])
 ni = np.array([2.0,4.0,2.0,2.0,2.0,2.0])
 Ki = np.array([2.89,0.91,0.02,1.55,1.72,1.89])
 
-### Central wavelengths for broad band filters (Aangstrom)
+# Central wavelengths for broad band filters (Aangstrom)
 lambdau=3540.
 lambdag=4750.
 lambdar=6220.
@@ -101,7 +105,7 @@ k = mags[:,8]
 #Build an array with the magnitudes
 magsar=np.array([u,g,r,i,zs,y,j,h,k])
             
-#Now run through the Nspec spectra
+#------------------------------ Now run through the Nspec spectra ---------------------------------------------------------------
 Nspec=18
 
 list = open('/home/christina/Desktop/QSOs/redspec1.list')
@@ -118,7 +122,6 @@ for s in range(0,Nspec):
     Naxis1 = spec[0].header['NAXIS1']
     wavelength = delta_lambda*np.arange(0,Naxis1)+lambda_central
     fluxe=flux[3]   #error specrum flux
-    #print fluxe
     flux=flux[0]
     
     #Convert mags to flux for each filter:
@@ -155,6 +158,7 @@ for s in range(0,Nspec):
     normc=np.mean(flx[filtc])       #normalized flux of comp spec without reddening
     
     #Correct the reddened composite spectrum for SMC reddening: 
+    
     Alambda = flx*0.      #Reset flux to zero
     for e in range(len(ai)):
         Alambda=Alambda+ai[e]/((wlr/wli[e])**ni[e]+(wli[e]/wlr)**ni[e]+bi[e])   #fitting function covering all wavelengths in microns
@@ -172,7 +176,7 @@ for s in range(0,Nspec):
     spece = fluxe/norm*deredr[2]*1e17   #Scale error spectrum to r-band
     spece=spece[0]
     
-    ########## PLOT ############
+    #-------------------------------------------- Plot the spectra --------------------------------------------------------------
     
     fig = plt.figure(figsize=(20,10))
     ax = fig.add_subplot(111)
@@ -182,144 +186,144 @@ for s in range(0,Nspec):
     ax4=fig.add_subplot(111) 
     ax5=fig.add_subplot(111) 
 
-    ax.set_xlabel("Wavelength (Angstrom)", fontsize=20)
-    ax.set_ylabel("Flux (10$^-17$ erg/cm$^2$/s/Angstrom)",fontsize=20)
-    ax.set_xticklabels(['5000','10000','20000'])
-    #plt.xticks(range(3), ('5000', '10000', '20000'))
+    ax.set_xlabel("Wavelength (Angstrom)", fontsize=25)
+    ax.set_ylabel("Flux (10$^-17$ erg/cm$^2$/s/Angstrom)",fontsize=25)
+    ax.set_xlim([3800,22000])
+    ax.tick_params(axis='both', which='both', labelsize=25)
+    ax.get_xaxis().set_major_formatter(ScalarFormatter())
 
-    #ax.semilogx(wavelength,specr,'k')
-    #ax1.semilogx(wavelength,spece,'b')    #error spectrum
-
-    
     if names[s] == str('BrightJ2103-0043'):
-        plt.text(15000,100,names[s], fontsize=20)
-        plt.text(15000,94, 'z=', fontsize=20)
-        plt.text(15800,94,zn, fontsize=20)
-        plt.text(17300,94,r'$A_B$=', fontsize=20)
-        plt.text(18600,94,ABn, fontsize=20)
-        ax.axis([3800,22000,0,110])
+        plt.text(15000,98,names[s], fontsize=25)
+        plt.text(15000,90, 'z=', fontsize=25)
+        plt.text(16000,90,zn, fontsize=25)
+        plt.text(18000,90,r'$A_B$=', fontsize=25)
+        plt.text(19800,90,ABn, fontsize=25)
+        #ax.axis([3800,22000,0,110])
+        ax.set_ylim([0,110])
         ax.semilogx(wavelength,specr,'k')
         ax1.semilogx(wavelength,spece,color='grey')    #error spectrum
         ax3.semilogx(wl,flx/normc*deredr[2]*1.e17,'g')   #composite spectrum without SMC reddening
         ax4.semilogx(wavar, deredr*1e17, 'ro', markersize=14)   #photometric points 
         ax5.semilogx(wl, model/normm*deredr[2]*1.e17,'r')    #composite spectrum with SMC reddening
     elif names[s] == str('RedJ2113-0028'):
-        plt.text(15000,9,names[s], fontsize=20)
-        plt.text(15000,7, 'z=', fontsize=20)
-        plt.text(15800,7,zn, fontsize=20)
-        plt.text(17300,7,r'$A_B$=', fontsize=20)
-        plt.text(18600,7,ABn, fontsize=20)
-        ax.axis([3800,22000,0,10])
+        plt.text(15000,8.8,names[s], fontsize=25)
+        plt.text(15000,7.9, 'z=', fontsize=25)
+        plt.text(16000,7.9,zn, fontsize=25)
+        plt.text(18000,7.9,r'$A_B$=', fontsize=25)
+        plt.text(19800,7.9,ABn, fontsize=25)
+        ax.set_ylim([0,10])
         ax.semilogx(wavelength,specr,'k')
         ax1.semilogx(wavelength,spece,color='grey')    #error spectrum
         ax3.semilogx(wl,flx/normc*deredr[2]*1.e17,'g')   #composite spectrum without SMC reddening
         ax4.semilogx(wavar, deredr*1e17, 'ro', markersize=14)   #photometric points 
         ax5.semilogx(wl, model/normm*deredr[2]*1.e17,'r')    #composite spectrum with SMC reddening
     elif names[s] == str('RedJ2045-0022'):
-        plt.text(15000,30,names[s], fontsize=20)
-        plt.text(15000,28, 'z=', fontsize=20)
-        plt.text(15800,28,zn, fontsize=20)
-        plt.text(17300,28,r'$A_B$=', fontsize=20)
-        plt.text(18600,28,ABn, fontsize=20)
-        ax.axis([3800,22000,0,32])
+        plt.text(15000,29,names[s], fontsize=25)
+        plt.text(15000,26, 'z=', fontsize=25)
+        plt.text(16000,26,zn, fontsize=25)
+        plt.text(18000,26,r'$A_B$=', fontsize=25)
+        plt.text(19800,26,ABn, fontsize=25)
+        ax.set_ylim([0,32])
         ax.semilogx(wavelength,specr,'k')
         ax1.semilogx(wavelength,spece,color='grey')    #error spectrum
         ax3.semilogx(wl,flx/normc*deredr[2]*1.e17,'g')   #composite spectrum without SMC reddening
         ax4.semilogx(wavar, deredr*1e17, 'ro', markersize=14)   #photometric points 
         ax5.semilogx(wl, model/normm*deredr[2]*1.e17,'r')    #composite spectrum with SMC reddening
     elif names[s] == str('RedJ0312+0032'):
-        plt.text(15000,13,names[s], fontsize=20)
-        plt.text(15000,11, 'z=', fontsize=20)
-        plt.text(15800,11,zn, fontsize=20)
-        plt.text(17300,11,r'$A_B$=', fontsize=20)
-        plt.text(18600,11,ABn, fontsize=20)
-        ax.axis([3800,22000,0,15])
+        plt.text(15000,13,names[s], fontsize=25)
+        plt.text(15000,11.7, 'z=', fontsize=25)
+        plt.text(16000,11.7,zn, fontsize=25)
+        plt.text(18000,11.7,r'$A_B$=', fontsize=25)
+        plt.text(19800,11.7,ABn, fontsize=25)
+        ax.set_ylim([0,15])
         ax.semilogx(wavelength,specr,'k')
         ax1.semilogx(wavelength,spece,color='grey')    #error spectrum
         ax3.semilogx(wl,flx/normc*deredr[2]*1.e17,'g')   #composite spectrum without SMC reddening
         ax4.semilogx(wavar, deredr*1e17, 'ro', markersize=14)   #photometric points 
         ax5.semilogx(wl, model/normm*deredr[2]*1.e17,'r')    #composite spectrum with SMC reddening
     elif names[s] == str('RedJ0312+0035'):
-        plt.text(15000,18,names[s], fontsize=20)
-        plt.text(15000,16, 'z=', fontsize=20)
-        plt.text(15800,16,zn, fontsize=20)
-        plt.text(17300,16,r'$A_B$=', fontsize=20)
-        plt.text(18600,16,ABn, fontsize=20)
-        ax.axis([3800,22000,0,20])   
+        plt.text(15000,18,names[s], fontsize=25)
+        plt.text(15000,16, 'z=', fontsize=25)
+        plt.text(16000,16,zn, fontsize=25)
+        plt.text(18000,16,r'$A_B$=', fontsize=25)
+        plt.text(19800,16,ABn, fontsize=25)
+        ax.set_ylim([0,20]) 
         ax.semilogx(wavelength,specr,'k')
         ax1.semilogx(wavelength,spece,color='grey')    #error spectrum
         ax3.semilogx(wl,flx/normc*deredr[2]*1.e17,'g')   #composite spectrum without SMC reddening
         ax4.semilogx(wavar, deredr*1e17, 'ro', markersize=14)   #photometric points 
         ax5.semilogx(wl, model/normm*deredr[2]*1.e17,'r')    #composite spectrum with SMC reddening
     elif names[s] == str('BrightJ2241-0103'):
-        plt.text(15000,56,names[s], fontsize=20)
-        plt.text(15000,53, 'z=', fontsize=20)
-        plt.text(15800,53,zn, fontsize=20)
-        plt.text(17300,53,r'$A_B$=', fontsize=20)
-        plt.text(18600,53,ABn, fontsize=20)
-        ax.axis([3800,22000,0,60])
+        plt.text(15000,54,names[s], fontsize=25)
+        plt.text(15000,49, 'z=', fontsize=25)
+        plt.text(16000,49,zn, fontsize=25)
+        plt.text(18000,49,r'$A_B$=', fontsize=25)
+        plt.text(19800,49,ABn, fontsize=25)
+        ax.set_ylim([0,60])
         ax.semilogx(wavelength,specr,'k')
         ax1.semilogx(wavelength,spece,color='grey')    #error spectrum
         ax3.semilogx(wl,flx/normc*deredr[2]*1.e17,'g')   #composite spectrum without SMC reddening
         ax4.semilogx(wavar, deredr*1e17, 'ro', markersize=14)   #photometric points 
         ax5.semilogx(wl, model/normm*deredr[2]*1.e17,'r')    #composite spectrum with SMC reddening
     elif names[s] == str('BrightJ2259+0103'):
-        plt.text(15000,56,names[s], fontsize=20)
-        plt.text(15000,53, 'z=', fontsize=20)
-        plt.text(15800,53,zn, fontsize=20)
-        plt.text(17300,53,r'$A_B$=', fontsize=20)
-        plt.text(18600,53,ABn, fontsize=20)
-        ax.axis([3800,22000,0,60])
+        plt.text(15000,54,names[s], fontsize=25)
+        plt.text(15000,49, 'z=', fontsize=25)
+        plt.text(16000,49,zn, fontsize=25)
+        plt.text(18000,49,r'$A_B$=', fontsize=25)
+        plt.text(19800,49,ABn, fontsize=25)
+        ax.set_ylim([0,60])
         ax.semilogx(wavelength,specr,'k')
         ax1.semilogx(wavelength,spece,color='grey')    #error spectrum
         ax3.semilogx(wl,flx/normc*deredr[2]*1.e17,'g')   #composite spectrum without SMC reddening
         ax4.semilogx(wavar, deredr*1e17, 'ro', markersize=14)   #photometric points 
         ax5.semilogx(wl, model/normm*deredr[2]*1.e17,'r')    #composite spectrum with SMC reddening
     elif names[s] == str('RedJ2320+0028'):
-        plt.text(15000,6,names[s], fontsize=20)
-        plt.text(15000,5, 'z=', fontsize=20)
-        plt.text(15800,5,zn, fontsize=20)
-        plt.text(17300,5,r'$A_B$=', fontsize=20)
-        plt.text(18600,5,ABn, fontsize=20)
-        ax.axis([3800,22000,0,7])
+        plt.text(15000,6.3,names[s], fontsize=25)
+        plt.text(15000,5.7, 'z=', fontsize=25)
+        plt.text(16000,5.7,zn, fontsize=25)
+        plt.text(18000,5.7,r'$A_B$=', fontsize=25)
+        plt.text(19800,5.7,ABn, fontsize=25)
+        ax.set_ylim([0,7])
         ax.semilogx(wavelength,specr,'k')
         ax1.semilogx(wavelength,spece,color='grey')    #error spectrum
         ax4.semilogx(wavar, deredr*1e17, 'ro', markersize=14)   #photometric points 
     elif names[s] == str('RedJ2047+0045'):
-        plt.text(15000,48,names[s], fontsize=20)
-        plt.text(15000,45, 'z=', fontsize=20)
-        plt.text(15800,45,zn, fontsize=20)
-        plt.text(17300,45,r'$A_B$=', fontsize=20)
-        plt.text(18600,45,ABn, fontsize=20)
-        ax.axis([3800,22000,0,50])
+        plt.text(15000,45,names[s], fontsize=25)
+        plt.text(15000,41, 'z=', fontsize=25)
+        plt.text(16000,41,zn, fontsize=25)
+        plt.text(18000,41,r'$A_B$=', fontsize=25)
+        plt.text(19800,41,ABn, fontsize=25)
+        ax.set_ylim([0,50])
         ax.semilogx(wavelength,specr,'k')
         ax1.semilogx(wavelength,spece,color='grey')    #error spectrum
         ax3.semilogx(wl,flx/normc*deredr[2]*1.e17,'g')   #composite spectrum without SMC reddening
         ax4.semilogx(wavar, deredr*1e17, 'ro', markersize=14)   #photometric points 
         ax5.semilogx(wl, model/normm*deredr[2]*1.e17,'r')    #composite spectrum with SMC reddening
     elif names[s] == str('BrightJ2043+0045'):
-        plt.text(15000,48,names[s], fontsize=20)
-        plt.text(15000,45, 'z=', fontsize=20)
-        plt.text(15800,45,zn, fontsize=20)
-        plt.text(17300,45,r'$A_B$=', fontsize=20)
-        plt.text(18600,45,ABn, fontsize=20)
-        ax.axis([3800,22000,0,52])
+        plt.text(15000,46,names[s], fontsize=25)
+        plt.text(15000,42, 'z=', fontsize=25)
+        plt.text(16000,42,zn, fontsize=25)
+        plt.text(18000,42,r'$A_B$=', fontsize=25)
+        plt.text(19800,42,ABn, fontsize=25)
+        ax.set_ylim([0,52])
         ax.semilogx(wavelength,specr,'k')
         ax1.semilogx(wavelength,spece,color='grey')    #error spectrum
         ax3.semilogx(wl,flx/normc*deredr[2]*1.e17,'g')   #composite spectrum without SMC reddening
         ax4.semilogx(wavar, deredr*1e17, 'ro', markersize=14)   #photometric points 
         ax5.semilogx(wl, model/normm*deredr[2]*1.e17,'r')    #composite spectrum with SMC reddening
+        
+#--------------------------------------  RedJ2048+0056 plotting procedure example -----------------------------------------------
+
     #elif names[s] == str('RedJ2048+0056'):
-        #plt.text(15000,30,names[s], fontsize=20)
-        #plt.text(5223,12, 'CIV', fontsize='18')
-        #plt.text(4200,15, r'Ly-$\alpha$', fontsize='18')
-        #plt.text(6436,15, r'CIII', fontsize='18')
-        #plt.text(9422,15, r'MgII', fontsize='18')
-        #plt.text(4720,15, r'SiIV', fontsize='18')
-        #plt.text(15000,28, 'z=', fontsize=20)
-        #plt.text(15800,28,zn, fontsize=20)
-        #plt.text(17300,28,r'$A_B$=', fontsize=20)
-        #plt.text(18600,28,ABn, fontsize=20)
+        #plt.text(15000,28,names[s], fontsize=25)
+        #plt.text(5223,12, 'CIV', fontsize='20')
+        #plt.text(4200,15, r'Ly-$\alpha$', fontsize='20')
+        #plt.text(6436,15, r'CIII', fontsize='20')
+        #plt.text(4720,15, r'SiIV', fontsize='20')
+        #plt.text(15000,25, 'z=', fontsize=25)
+        #plt.text(16000,25,zn, fontsize=25)
+        #plt.text(18000,25,r'$A_B$=', fontsize=25)
+        #plt.text(19800,25,ABn, fontsize=25)
         #ax.axis([3800,22000,0,32])
         #ax.semilogx(wavelength,specr,'k')
         #ax1.semilogx(wavelength,spece,color='grey')    #error spectrum
@@ -332,23 +336,20 @@ for s in range(0,Nspec):
         #l4=[6.5,32]
         #l5=[6429,6429]
         #l6=[6.5,32]
-        #l7=[9417,9417]
+        #l7=[4715,4715]
         #l8=[6.5,32]
-        #l9=[4715,4715]
-        #l10=[6.5,32]
         #plt.plot(l1,l2,'--', color = 'grey')
         #plt.plot(l3,l4,'--', color = 'grey')
         #plt.plot(l5,l6,'--', color = 'grey')
         #plt.plot(l7,l8,'--', color = 'grey')
-        #plt.plot(l9,l10,'--', color = 'grey')
-        #ax3.semilogx(wl,flx/normc*deredr[2]*1.e17,'g')   #composite spectrum without SMC reddening
+        #plt.savefig("/home/christina/Desktop/QSOs/Figures/RedJ2048c.pdf") 
     else:
-        plt.text(15000,30,names[s], fontsize=20)
-        plt.text(15000,28, 'z=', fontsize=20)
-        plt.text(15800,28, zn, fontsize=20)
-        plt.text(17300,28,r'$A_B$=', fontsize=20)
-        plt.text(18600,28,ABn, fontsize=20)
-        ax.axis([3800,22000,0,32])
+        plt.text(15000,28,names[s], fontsize=25)
+        plt.text(15000,25, 'z=', fontsize=25)
+        plt.text(16000,25, zn, fontsize=25)
+        plt.text(18000,25,r'$A_B$=', fontsize=25)
+        plt.text(19800,25,ABn, fontsize=25)
+        ax.set_ylim([0,32])
         ax.semilogx(wavelength,specr,'k')
         ax1.semilogx(wavelength,spece,color='grey')    #error spectrum
         ax3.semilogx(wl,flx/normc*deredr[2]*1.e17,'g')   #composite spectrum without SMC reddening
@@ -359,12 +360,32 @@ for s in range(0,Nspec):
  
    #Composite spectrum
     ax2=ax.twiny()
-    ax2.set_xlabel("Rest Wavelength (Angstrom)", fontsize=20)
+    #ax2.xticks([1000,2000,3000,4000,5000],fontsize=14)
+    wln=comp_file[:,0]
+    
+    ax2.semilogx(wln,flx/normc*deredr[2]*1.e17, color='white',linestyle="None")
+    ax2.set_xlabel("Rest Wavelength (Angstrom)", fontsize=25)
+    ax2.xaxis.set_major_formatter(ScalarFormatter())
+    ax2.ticklabel_format(axis='both', style='plain', fontsize=25)
+    plt.xticks(fontsize=25)
     #plt.setp(ax,xticks=[5000, 10000, 20000], xticklabels=['5000', '10000', '20000']) 
-    #plt.xticks(np.arange(3), ('1000', '2000', '5000'))
+    #ax2.set_xticks(np.arange(3), ('1000', '2000', '5000'))
     #ax3.semilogx(wl,flx/normc*deredr[2]*1.e17,'g')   #composite spectrum without SMC reddening
     #ax4.semilogx(wavar, deredr*1e17, 'ro', markersize=14)   #photometric points 
     #ax5.semilogx(wl, model/normm*deredr[2]*1.e17,'r')    #composite spectrum with SMC reddening
-  
+    plt.savefig("/home/christina/Desktop/QSOs/Figures/"+ names[s] + '.pdf')        #save all figures as pdf 
     plt.show()
 list.close()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
